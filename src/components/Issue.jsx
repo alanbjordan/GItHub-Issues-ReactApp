d481c6114eb4ac686f1a51735d72444f8a1731eb
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-30 12:24:23 -0400

    created Issue jsx file

---
+++ src/components/Issue.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import IssueList from './IssueList'
+
+const Issue = (props) => {
+    // const { title } = props;
+
+    return (
+        <div>
+            <h3>Issue:</h3>{props.issue.title}
+            <p><a href={props.issue.html_url}>Issue Link</a></p>
+        </div>
+    )
+}
+
+export default Issue;
