390dcdfd4d6fbd0afe16ab672a46cf6ae30139d0
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-30 12:22:58 -0400

    updated functional component to be a simple passthru

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import IssueList from './components/IssueList'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Blah
-      <IssueList />
-      </header>
+        <IssueList/>
+      <p>hello world</p>
     </div>
   );
 }
