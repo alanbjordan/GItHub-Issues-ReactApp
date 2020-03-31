43e24d9c32f82ede3dfcc2085e4fcdd75f3a7666
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-31 14:52:01 -0400

    Refactor
    Changed the issue component to render specific issue

--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import IssueList from './IssueList'
+import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
 
-const Issue = (props) => {
-    // const { title } = props;
+class Issue extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            issues: [],
+        };
+    }
 
-    return (
-        <div>
-            <h3>Issue:</h3>{props.issue.title}
-            <p><a href={props.issue.html_url}>Issue Link</a></p>
-        </div>
-    )
+    componentDidMount() {
+        const number = this.props.match.params.issue_number;
+        console.log(number);
+        fetch(`https://api.github.com/repos/facebook/create-react-app/issues/${number}`)
+            .then(res => res.json())
+            .then((result) => {
+                this.setState({
+                    issues: result
+                });
+            });
+    }
+    
+    render() {
+        console.log("returning data: ", this.state.issues)
+        const data  = this.state.issues;
+        return (
+            <div>
+                <div>
+                    <h3>{data.title}</h3>                    
+                </div>
+                <div>
+                    <ReactMarkdown source={data.body} escapeHtml={false}/>                  
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Issue;
