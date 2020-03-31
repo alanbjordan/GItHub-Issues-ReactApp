25bc4c07f6fc46548a526e6bb1636a44cd2a72c4
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-31 14:50:27 -0400

    Refactoring
    Import Issue componenet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
-import IssueList from './components/IssueList'
+import IssueList from './components/IssueList';
+import Issue from './components/Issue';
+import './components/issues.css';
+import { 
+  Route,
+  BrowserRouter as Router,
+} from 'react-router-dom';
+
 
 function App() {
 
   return (
     <div className="App">
-        <IssueList/>
-      <p>hello world</p>
+      <Router>
+          <Route path='/' exact component={IssueList} />
+          <Route path='/issue/:issue_number?' component={Issue}/>
+      </Router>
     </div>
   );
 }
-
 export default App;
